771a8a95b40563af2b4b4300306d1cf172be0d1e
Tan Le <lenhattan2313@gmail.com>
2022-10-18 11:03:13 +0700

    Implement navbar & hero section

---
+++ components/GetStarted.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+import { arrowUp } from "../assets";
+import styles from "../styles/styles";
+
+const GetStarted = () => {
+  return (
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    >
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}
+      >
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+            <span className="text-gradient">Get</span>
+          </p>
+          <Image src={arrowUp} className="w-[23px] object-contain" alt="hero" />
+        </div>
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
